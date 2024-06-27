5095b05c3b4ca23c3afa07a0c472a0436560ca25
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2024-06-27 12:49:18 +0300

    V1.0

--- app/src/main/java/com/example/grad_project/MainActivity.kt
+++ app/src/main/java/com/example/grad_project/MainActivity.kt
@@ -8,6 +8,7 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.SurfaceTexture
+import android.graphics.Typeface
 import android.hardware.camera2.CameraCaptureSession
 import android.hardware.camera2.CameraDevice
 import android.hardware.camera2.CameraManager
@@ -15,6 +16,7 @@ import android.media.MediaPlayer
 import android.os.Bundle
 import android.os.Handler
 import android.os.HandlerThread
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.Surface
 import android.view.TextureView
@@ -60,7 +62,11 @@ class MainActivity : AppCompatActivity() {
         handlerThread.start()
         handler = Handler(handlerThread.looper)
 
-        paint.color = Color.YELLOW
+        paint.apply {
+            color = Color.YELLOW
+            textSize = 20f // Set text size to make the font bigger
+            typeface = Typeface.create(Typeface.DEFAULT, Typeface.BOLD) // Set text style to bold
+        }
 
         // Initialize media player for warning sound
         mediaPlayer = MediaPlayer.create(this, R.raw.warning)
@@ -100,7 +106,7 @@ class MainActivity : AppCompatActivity() {
                 // Draw key points and check posture
                 val keyPoints = mutableListOf<FloatArray>()
                 for (i in outputFeature0.indices step 3) {
-                    if (outputFeature0[i + 2] > 0.25) {
+                    if (outputFeature0[i + 2] > 0.2) {
                         val x = outputFeature0[i + 1] * w
                         val y = outputFeature0[i] * h
                         canvas.drawCircle(x, y, 10f, paint)
@@ -115,7 +121,7 @@ class MainActivity : AppCompatActivity() {
 
                 // Check posture
                 val posture = checkPosture(keyPoints, canvas)
-                if (posture == "bad") {
+                if (posture == "bad_sitting") {
                     badPostureCount++
                     if (badPostureCount >= 20) {
                         if (!mediaPlayer.isPlaying) {
@@ -187,73 +193,143 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun checkPosture(keyPoints: List<FloatArray>, canvas: Canvas): String {
-        // Assuming keyPoints[1] is left ear, keyPoints[2] is right ear, keyPoints[5] is left shoulder, keyPoints[6] is right shoulder, keyPoints[11] is left hip, keyPoints[12] is right hip
-
-        val leftEar = keyPoints[1]
-        val rightEar = keyPoints[2]
+        val nose = keyPoints[0]
+        val leftEye = keyPoints[1]
+        val rightEye = keyPoints[2]
+        val leftEar = keyPoints[3]
+        val rightEar = keyPoints[4]
         val leftShoulder = keyPoints[5]
         val rightShoulder = keyPoints[6]
+        val leftElbow = keyPoints[7]
+        val rightElbow = keyPoints[8]
+        val leftWrist = keyPoints[9]
+        val rightWrist = keyPoints[10]
         val leftHip = keyPoints[11]
         val rightHip = keyPoints[12]
+        val leftKnee = keyPoints[13]
+        val rightKnee = keyPoints[14]
+        val leftAnkle = keyPoints[15]
+        val rightAnkle = keyPoints[16]
 
-        // Check if all required key points are detected
-        if (leftEar.any { it.isNaN() } || rightEar.any { it.isNaN() } || leftShoulder.any { it.isNaN() } || rightShoulder.any { it.isNaN() } || leftHip.any { it.isNaN() } || rightHip.any { it.isNaN() }) {
-            return "good" // Not all key points are detected
+        // Check if key points for posture detection are available
+        if (leftEar.any { it.isNaN() } && rightEar.any { it.isNaN() } || leftShoulder.any { it.isNaN() } &&
+            rightShoulder.any { it.isNaN() } || leftHip.any { it.isNaN() } && rightHip.any { it.isNaN() } ||
+            leftKnee.any { it.isNaN() } && rightKnee.any { it.isNaN() }) {
+            Log.d("Undetected", "Not complete")
+            return "good"
         }
 
-        // Calculate neck and torso inclinations
+        // Calculate angles and inclinations
         val neckInclination = calculateInclination(leftEar, leftShoulder)
         val torsoInclination = calculateInclination(leftShoulder, leftHip)
+        val shoulderInclination = calculateInclination(leftShoulder, rightShoulder)
+        val headTilt = calculateInclination(leftEar, rightEar)
+        val hipInclination = calculateInclination(leftHip, rightHip)
+        val leftElbowAngle = calculateAngle(leftShoulder, leftElbow, leftWrist)
+        val rightElbowAngle = calculateAngle(rightShoulder, rightElbow, rightWrist)
+        val leftKneeAngle = calculateAngle(leftHip, leftKnee, leftAnkle)
+        val rightKneeAngle = calculateAngle(rightHip, rightKnee, rightAnkle)
 
-        // Display angles and draw lines
+        // List to store drawn text positions to avoid overlap
+        val drawnTextPositions = mutableListOf<Pair<Float, Float>>()
 
-        val lEarX = leftEar[0].toInt()
-        val lEarY = leftEar[1].toInt()
-        val lShldrX = leftShoulder[0].toInt()
-        val lShldrY = leftShoulder[1].toInt()
-        val lHipX = leftHip[0].toInt()
-        val lHipY = leftHip[1].toInt()
+        // Draw angles on the canvas
+        drawAngle(canvas, leftShoulder, leftEar, neckInclination, "Neck", drawnTextPositions)
+        drawAngle(canvas, leftHip, leftShoulder, torsoInclination, "Torso", drawnTextPositions)
+        drawAngle(canvas, leftShoulder, rightShoulder, shoulderInclination, "Shoulders", drawnTextPositions)
+        drawAngle(canvas, leftHip, rightHip, hipInclination, "Hips", drawnTextPositions)
+        drawAngle(canvas, leftEar, rightEar, headTilt, "Head Tilt", drawnTextPositions)
+        drawAngle(canvas, leftElbow, leftShoulder, leftElbowAngle, "L-Elbow", drawnTextPositions)
+        drawAngle(canvas, rightElbow, rightShoulder, rightElbowAngle, "R-Elbow", drawnTextPositions)
+        drawAngle(canvas, leftKnee, leftHip, leftKneeAngle, "L-Knee", drawnTextPositions)
+        drawAngle(canvas, rightKnee, rightHip, rightKneeAngle, "R-Knee", drawnTextPositions)
 
-        val angleTextString = "Neck: %.2f, Torso: %.2f".format(neckInclination, torsoInclination)
-        canvas.drawText(angleTextString, 10f, 30f, paint)
-        canvas.drawText(neckInclination.toInt().toString(), lShldrX + 10f, lShldrY.toFloat(), paint)
-        canvas.drawText(torsoInclination.toInt().toString(), lHipX + 10f, lHipY.toFloat(), paint)
+        // Check if it is a sitting posture
+        val sittingPosture = isSittingPosture(leftHip, rightHip, leftKnee, rightKnee)
 
-        canvas.drawLine(lShldrX.toFloat(), lShldrY.toFloat(), lEarX.toFloat(), lEarY.toFloat(), paint)
-        canvas.drawLine(lShldrX.toFloat(), lShldrY.toFloat(), lShldrX.toFloat(), lShldrY - 100f, paint)
-        canvas.drawLine(lHipX.toFloat(), lHipY.toFloat(), lShldrX.toFloat(), lShldrY.toFloat(), paint)
-        canvas.drawLine(lHipX.toFloat(), lHipY.toFloat(), lHipX.toFloat(), lHipY - 100f, paint)
+        if (!sittingPosture) {
+            Log.d("Not_Sitting", "Not sitting")
+            return "not_sitting"
+        }
 
-        return if (neckInclination in 75.0..115.0 && torsoInclination <= 100 && torsoInclination >= 80) "good" else "bad"
+        Log.d("Neck", neckInclination.toString())
+        Log.d("Terso", torsoInclination.toString())
+        // Determine if sitting posture is good or bad
+        return if (neckInclination in 85.0..95.0 && torsoInclination <= 100 && torsoInclination >= 80) "good_sitting" else "bad_sitting"
+    }
+
+    private fun isSittingPosture(leftHip: FloatArray, rightHip: FloatArray, leftKnee: FloatArray, rightKnee: FloatArray): Boolean {
+        val leftHipKneeAngle = calculateAngle(leftHip, leftKnee, floatArrayOf(leftKnee[0], leftKnee[1] + 1))
+        val rightHipKneeAngle = calculateAngle(rightHip, rightKnee, floatArrayOf(rightKnee[0], rightKnee[1] + 1))
+
+        return leftHipKneeAngle in 70.0..150.0 || rightHipKneeAngle in 70.0..150.0
     }
 
     private fun calculateInclination(pointA: FloatArray, pointB: FloatArray): Float {
-        val deltaX = pointA[0] - pointB[0]
-        val deltaY = pointA[1] - pointB[1]
-        val angle = Math.toDegrees(atan2(deltaY.toDouble(), deltaX.toDouble())).toFloat()
-        return if (angle < 0) -1 * angle else angle
+        val angle = Math.toDegrees(atan2((pointB[1] - pointA[1]).toDouble(), (pointB[0] - pointA[0]).toDouble())).toFloat()
+        return if (angle < 0) angle + 360 else angle
+    }
+
+    private fun calculateAngle(pointA: FloatArray, pointB: FloatArray, pointC: FloatArray): Float {
+        var angle = Math.toDegrees(atan2(pointC[1] - pointB[1], pointC[0] - pointB[0]).toDouble() -
+                atan2(pointA[1] - pointB[1], pointA[0] - pointB[0]).toDouble()).toFloat()
+        if (angle < 0) {
+            angle += 360
+        }
+        return if (angle > 180) 360 - angle else angle
+
+    }
+
+    private fun drawAngle(canvas: Canvas, startPoint: FloatArray, endPoint: FloatArray, angle: Float, label: String, drawnTextPositions: MutableList<Pair<Float, Float>>) {
+        val startX = startPoint[0]
+        val startY = startPoint[1]
+        val endX = endPoint[0]
+        val endY = endPoint[1]
+
+        // Draw the line between start and end points
+        canvas.drawLine(startX, startY, endX, endY, paint)
+
+        // Initial position for text
+        var textX = startX + 10
+        var textY = startY - 10
+
+        // Avoid overlapping text
+        val textHeight = paint.textSize
+        val textWidth = paint.measureText("${angle.toInt()}° $label")
+
+        var isOverlapping: Boolean
+        var attempts = 0
+        do {
+            isOverlapping = false
+            for (position in drawnTextPositions) {
+                val distanceX = Math.abs(textX - position.first)
+                val distanceY = Math.abs(textY - position.second)
+                if (distanceX < textWidth && distanceY < textHeight) {
+                    isOverlapping = true
+                    textY += textHeight + 5 // Move the text down by text height + margin
+                    break
+                }
+            }
+            attempts++
+        } while (isOverlapping && attempts < 10)
+
+        paint.color = Color.RED
+        canvas.drawText("${angle.toInt()}° $label", textX, textY, paint)
+        paint.color = Color.YELLOW
+        drawnTextPositions.add(Pair(textX, textY))
     }
 
     private fun drawSkeleton(canvas: Canvas, keyPoints: List<FloatArray>) {
-        val connections = listOf(
-            Pair(5, 7), // Left shoulder to left elbow
-            Pair(7, 9), // Left elbow to left wrist
-            Pair(6, 8), // Right shoulder to right elbow
-            Pair(8, 10), // Right elbow to right wrist
-            Pair(5, 11), // Left shoulder to left hip
-            Pair(6, 12), // Right shoulder to right hip
-            Pair(11, 13), // Left hip to left knee
-            Pair(13, 15), // Left knee to left ankle
-            Pair(12, 14), // Right hip to right knee
-            Pair(14, 16)  // Right knee to right ankle
+        val skeleton = listOf(
+            Pair(5, 6), Pair(5, 7), Pair(7, 9), Pair(6, 8), Pair(8, 10),
+            Pair(5, 11), Pair(6, 12), Pair(11, 12), Pair(11, 13), Pair(13, 15),
+            Pair(12, 14), Pair(14, 16)
         )
-
-        for (connection in connections) {
-            val start = keyPoints[connection.first]
-            val end = keyPoints[connection.second]
-
-            if (start.all { !it.isNaN() } && end.all { !it.isNaN() }) {
-                canvas.drawLine(start[0], start[1], end[0], end[1], paint)
+        for ((startIdx, endIdx) in skeleton) {
+            val startPoint = keyPoints[startIdx]
+            val endPoint = keyPoints[endIdx]
+            if (!startPoint.any { it.isNaN() } && !endPoint.any { it.isNaN() }) {
+                canvas.drawLine(startPoint[0], startPoint[1], endPoint[0], endPoint[1], paint)
             }
         }
     }
